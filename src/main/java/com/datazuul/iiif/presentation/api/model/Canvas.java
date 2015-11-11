0de5ab2e6f3624b9a7fa7669dbfc9a3d99eddf00
Ralf Eichinger <ralf.eichinger@bsb-muenchen.de>
2015-11-11 16:56:38 +0100

    json deserialization needs default constructor

--- src/main/java/com/datazuul/iiif/presentation/api/model/Canvas.java
+++ src/main/java/com/datazuul/iiif/presentation/api/model/Canvas.java
@@ -39,114 +39,132 @@ import java.util.List;
  */
 public class Canvas extends AbstractIiifResource {
 
-    private String description; // optional
-    private final int height; // required
-    private List<Image> images;
-    private final String label; // required
-    private List<Metadata> metadata; // optional
-    private String thumbnail; // recommended
-    private String viewingHint; // optional
-    private final int width; // required
+  private String description; // optional
+  private int height; // required
+  private List<Image> images;
+  private String label; // required
+  private List<Metadata> metadata; // optional
+  private String thumbnail; // recommended
+  private String viewingHint; // optional
+  private int width; // required
 
-    public Canvas(String id, String label, int height, int width) {
-        assert id != null;
-        assert label != null;
-        assert height > -1;
-        assert width > -1;
+  public void setHeight(int height) {
+    this.height = height;
+  }
 
-        this.id = id;
-        this.height = height;
-        this.label = label;
-        this.width = width;
+  public void setLabel(String label) {
+    this.label = label;
+  }
 
-        type = "sc:Canvas";
-    }
+  public void setWidth(int width) {
+    this.width = width;
+  }
 
-    /**
-     * @param id A canvas must have an id, and it must be an http(s) URI. The canvas’s JSON representation should be
-     * published at that URI.
-     * @param label A canvas must have a label, and it should be the page or view label such as “p. 1”, “front”, or
-     * “north view”.
-     * @param height The height of a canvas or image resource. For images, this is in pixels. No particular units are
-     * required for canvases, as the dimensions provide an aspect ratio for the resources to be located within rather
-     * than measuring any physical property of the object. A canvas must have a height, which does not have a unit type.
-     * It merely conveys, along with width, an aspect ratio.
-     * @param width The width of a canvas or image resource. For images, this is in pixels. No particular units are
-     * required for canvases. A canvas must have a height, which does not have a unit type. It merely conveys, along
-     * with width, an aspect ratio.
-     * @param thumbnail A canvas may have a thumbnail and should have a thumbnail if there are multiple images or
-     * resources that make up the representation.
-     */
-    public Canvas(String id, String label, int height, int width, String thumbnail) {
-        this(id, label, height, width);
-        this.thumbnail = thumbnail;
-    }
+  public Canvas() {
+    this.height = 0;
+    this.label = null;
+    this.width = 0;
+  }
 
-    public String getDescription() {
-        return description;
-    }
+  public Canvas(String id, String label, int height, int width) {
+    assert id != null;
+    assert label != null;
+    assert height > -1;
+    assert width > -1;
 
-    /**
-     * @param description A canvas may have a description to describe particular features of the view.
-     */
-    public void setDescription(String description) {
-        this.description = description;
-    }
+    this.id = id;
+    this.height = height;
+    this.label = label;
+    this.width = width;
 
-    public int getHeight() {
-        return height;
-    }
+    type = "sc:Canvas";
+  }
 
-    public List<Image> getImages() {
-        return images;
-    }
+  /**
+   * @param id A canvas must have an id, and it must be an http(s) URI. The canvas’s JSON representation should be
+   * published at that URI.
+   * @param label A canvas must have a label, and it should be the page or view label such as “p. 1”, “front”, or “north
+   * view”.
+   * @param height The height of a canvas or image resource. For images, this is in pixels. No particular units are
+   * required for canvases, as the dimensions provide an aspect ratio for the resources to be located within rather than
+   * measuring any physical property of the object. A canvas must have a height, which does not have a unit type. It
+   * merely conveys, along with width, an aspect ratio.
+   * @param width The width of a canvas or image resource. For images, this is in pixels. No particular units are
+   * required for canvases. A canvas must have a height, which does not have a unit type. It merely conveys, along with
+   * width, an aspect ratio.
+   * @param thumbnail A canvas may have a thumbnail and should have a thumbnail if there are multiple images or
+   * resources that make up the representation.
+   */
+  public Canvas(String id, String label, int height, int width, String thumbnail) {
+    this(id, label, height, width);
+    this.thumbnail = thumbnail;
+  }
 
-    public void setImages(List<Image> images) {
-        this.images = images;
-    }
+  public String getDescription() {
+    return description;
+  }
 
-    public String getLabel() {
-        return label;
-    }
+  /**
+   * @param description A canvas may have a description to describe particular features of the view.
+   */
+  public void setDescription(String description) {
+    this.description = description;
+  }
 
-    public List<Metadata> getMetadata() {
-        return metadata;
-    }
+  public int getHeight() {
+    return height;
+  }
 
-    /**
-     * @param metadata A canvas may have metadata pairs associated with it to describe its particular features.
-     */
-    public void setMetadata(List<Metadata> metadata) {
-        this.metadata = metadata;
-    }
+  public List<Image> getImages() {
+    return images;
+  }
 
-    public String getThumbnail() {
-        return thumbnail;
-    }
+  public void setImages(List<Image> images) {
+    this.images = images;
+  }
 
-    /**
-     * @param thumbnail A canvas may have a thumbnail and should have a thumbnail if there are multiple images or
-     * resources that make up the representation.
-     */
-    public void setThumbnail(String thumbnail) {
-        this.thumbnail = thumbnail;
-    }
+  public String getLabel() {
+    return label;
+  }
 
-    public String getViewingHint() {
-        return viewingHint;
-    }
+  public List<Metadata> getMetadata() {
+    return metadata;
+  }
 
-    /**
-     * @param viewingHint A manifest, sequence or range may have a viewing hint, with scope as per viewingDirection. A
-     * canvas may have a viewing hint, and the only hint defined by this specification for canvases is “non-paged”.
-     * “non-paged” is only valid if the canvas is within a manifest, sequence or range that is “paged”, and the
-     * particular canvas must not be displayed in a page-turning viewer.
-     */
-    public void setViewingHint(String viewingHint) {
-        this.viewingHint = viewingHint;
-    }
+  /**
+   * @param metadata A canvas may have metadata pairs associated with it to describe its particular features.
+   */
+  public void setMetadata(List<Metadata> metadata) {
+    this.metadata = metadata;
+  }
 
-    public int getWidth() {
-        return width;
-    }
+  public String getThumbnail() {
+    return thumbnail;
+  }
+
+  /**
+   * @param thumbnail A canvas may have a thumbnail and should have a thumbnail if there are multiple images or
+   * resources that make up the representation.
+   */
+  public void setThumbnail(String thumbnail) {
+    this.thumbnail = thumbnail;
+  }
+
+  public String getViewingHint() {
+    return viewingHint;
+  }
+
+  /**
+   * @param viewingHint A manifest, sequence or range may have a viewing hint, with scope as per viewingDirection. A
+   * canvas may have a viewing hint, and the only hint defined by this specification for canvases is “non-paged”.
+   * “non-paged” is only valid if the canvas is within a manifest, sequence or range that is “paged”, and the particular
+   * canvas must not be displayed in a page-turning viewer.
+   */
+  public void setViewingHint(String viewingHint) {
+    this.viewingHint = viewingHint;
+  }
+
+  public int getWidth() {
+    return width;
+  }
 }
