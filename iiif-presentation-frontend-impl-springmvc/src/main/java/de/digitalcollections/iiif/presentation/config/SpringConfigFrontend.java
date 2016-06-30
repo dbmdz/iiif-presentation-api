4622d98e4176798308ceb1280cdd2e4270f61847
Ralf Eichinger <ralf.eichinger@bsb-muenchen.de>
2016-06-30 10:15:31 +0200

    added frontend impl spring mvc module

---
+++ iiif-presentation-frontend-impl-springmvc/src/main/java/de/digitalcollections/iiif/presentation/config/SpringConfigFrontend.java
@@ -0,0 +1,64 @@
+package de.digitalcollections.iiif.presentation.config;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import de.digitalcollections.iiif.presentation.model.impl.jackson.IiifPresentationApiObjectMapper;
+import java.nio.charset.Charset;
+import java.util.ArrayList;
+import java.util.List;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.MediaType;
+import org.springframework.http.converter.ByteArrayHttpMessageConverter;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.http.converter.StringHttpMessageConverter;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+/**
+ * IIIF specific configuration.
+ */
+@Configuration
+@ComponentScan(basePackages = {
+  "de.digitalcollections.iiif.presentation.frontend.impl.springmvc.controller"
+})
+@EnableWebMvc
+public class SpringConfigFrontend extends WebMvcConfigurerAdapter {
+
+  @Override
+  public void configureMessageConverters(List<HttpMessageConverter<?>> converters) {
+    // support for @ResponseBody of type String
+    final StringHttpMessageConverter stringHMC = new StringHttpMessageConverter(Charset.
+            forName("UTF-8"));
+    // supported MediaTypes for stringHMC are by default set to: "text/plain" and MediaType.ALL
+    converters.add(stringHMC);
+
+    // support for @ResponseBody of type Object: convert object to JSON
+    // used in ApiController
+    converters.add(mappingJackson2HttpMessageConverter());
+
+    // support for @ResponseBody of type byte[]
+    ByteArrayHttpMessageConverter bc = new ByteArrayHttpMessageConverter();
+    List<MediaType> supported = new ArrayList<>();
+    supported.add(MediaType.IMAGE_JPEG);
+    bc.setSupportedMediaTypes(supported);
+    converters.add(bc);
+  }
+
+  @Bean
+  public HttpMessageConverter<?> mappingJackson2HttpMessageConverter() {
+    MappingJackson2HttpMessageConverter converter = new MappingJackson2HttpMessageConverter();
+    converter.setObjectMapper(objectMapper());
+    return converter;
+  }
+
+  @Bean
+  public ObjectMapper objectMapper() {
+    ObjectMapper objectMapper = new IiifPresentationApiObjectMapper();
+    // define which fields schould be ignored with Filter-classes:
+//        objectMapper.addMixIn(User.class, UserJsonFilter.class);
+//        objectMapper.addMixIn(GrantedAuthority.class, GrantedAuthorityJsonFilter.class);
+    return objectMapper;
+  }
+}
