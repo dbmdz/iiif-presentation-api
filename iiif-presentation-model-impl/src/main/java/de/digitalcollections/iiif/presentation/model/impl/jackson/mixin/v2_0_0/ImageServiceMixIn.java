9173d22e69362ddede582a37e8cc35ca127ff426
Johannes Baiter <johannes.baiter@bsb-muenchen.de>
2016-09-29 12:05:02 +0200

    Ignore width/height on services during serialization if not set

--- iiif-presentation-model-impl/src/main/java/de/digitalcollections/iiif/presentation/model/impl/jackson/mixin/v2_0_0/ImageServiceMixIn.java
+++ iiif-presentation-model-impl/src/main/java/de/digitalcollections/iiif/presentation/model/impl/jackson/mixin/v2_0_0/ImageServiceMixIn.java
@@ -1,9 +1,17 @@
 package de.digitalcollections.iiif.presentation.model.impl.jackson.mixin.v2_0_0;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+
 import de.digitalcollections.iiif.presentation.model.impl.v2_0_0.ImageServiceImpl;
 
 @JsonDeserialize(as = ImageServiceImpl.class)
 public abstract class ImageServiceMixIn extends ServiceMixIn {
 
+  @JsonInclude(JsonInclude.Include.NON_DEFAULT)
+  abstract int getHeight();
+
+  @JsonInclude(JsonInclude.Include.NON_DEFAULT)
+  abstract int getWidth();
 }
