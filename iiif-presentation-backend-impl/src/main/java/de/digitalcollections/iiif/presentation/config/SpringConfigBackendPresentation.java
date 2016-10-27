ac93bf592e0aecf3e965e45d3bbf7e677d38c79a
Ralf Eichinger <ralf.eichinger@bsb-muenchen.de>
2016-10-27 17:19:35 +0200

    replaced file backend with "digitalcollections core backend impl file"

--- iiif-presentation-backend-impl/src/main/java/de/digitalcollections/iiif/presentation/config/SpringConfigBackendPresentation.java
+++ iiif-presentation-backend-impl/src/main/java/de/digitalcollections/iiif/presentation/config/SpringConfigBackendPresentation.java
@@ -1,30 +1,25 @@
 package de.digitalcollections.iiif.presentation.config;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.PropertySource;
-import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
 
 /**
  * Backend configuration.
  */
 @Configuration
 @ComponentScan(basePackages = {
-  "de.digitalcollections.iiif.presentation.backend.impl.repository",
-  "de.digitalcollections.iiif.presentation.backend.impl.resolver"
+  "de.digitalcollections.core.config",
+  "de.digitalcollections.iiif.presentation.backend.impl.repository"
 })
-@PropertySource(value = {
-  "classpath:de/digitalcollections/iiif/presentation/config/SpringConfigBackend-${spring.profiles.active:PROD}.properties"
-})
+//@PropertySource(value = {
+//  "classpath:de/digitalcollections/iiif/presentation/config/SpringConfigBackend-${spring.profiles.active:PROD}.properties"
+//})
 public class SpringConfigBackendPresentation {
 
-  private static final Logger LOGGER = LoggerFactory.getLogger(SpringConfigBackendPresentation.class);
-
-  @Bean
-  public static PropertySourcesPlaceholderConfigurer propertySourcesPlaceholderConfigurer() {
-    return new PropertySourcesPlaceholderConfigurer();
-  }
+//  private static final Logger LOGGER = LoggerFactory.getLogger(SpringConfigBackendPresentation.class);
+//
+//  @Bean
+//  public static PropertySourcesPlaceholderConfigurer propertySourcesPlaceholderConfigurer() {
+//    return new PropertySourcesPlaceholderConfigurer();
+//  }
 }
