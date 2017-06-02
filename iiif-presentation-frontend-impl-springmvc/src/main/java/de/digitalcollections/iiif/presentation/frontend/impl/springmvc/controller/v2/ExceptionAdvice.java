ddda06de0e712be15fd3f53fd938c411a399bc97
Ralf Eichinger <ralf.eichinger@bsb-muenchen.de>
2017-06-02 15:59:20 +0200

    Add exception logging

--- iiif-presentation-frontend-impl-springmvc/src/main/java/de/digitalcollections/iiif/presentation/frontend/impl/springmvc/controller/v2/ExceptionAdvice.java
+++ iiif-presentation-frontend-impl-springmvc/src/main/java/de/digitalcollections/iiif/presentation/frontend/impl/springmvc/controller/v2/ExceptionAdvice.java
@@ -1,6 +1,8 @@
 package de.digitalcollections.iiif.presentation.frontend.impl.springmvc.controller.v2;
 
 import de.digitalcollections.iiif.presentation.model.api.exceptions.NotFoundException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -8,6 +10,9 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 
 @ControllerAdvice
 public class ExceptionAdvice {
+
+  private static final Logger LOGGER = LoggerFactory.getLogger(ExceptionAdvice.class);
+
   @ResponseStatus(HttpStatus.NOT_FOUND)
   @ExceptionHandler(NotFoundException.class)
   public void handleNotFound() {
@@ -15,6 +20,7 @@ public class ExceptionAdvice {
 
   @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
   @ExceptionHandler(Exception.class)
-  public void handleAllOther() {
+  public void handleAllOther(Exception exception) {
+    LOGGER.error("exception stack trace", exception);
   }
 }
